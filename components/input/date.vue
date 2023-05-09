4d1e9742ac6600ef031a469763879f44a5552195
leongjs98 <leon_gjs@protonmail.com>
2023-05-09 19:59:35 +0800

    Added basic create member page

---
+++ components/input/date.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex items-center gap-4">
+    <div class="flex w-20 justify-center items-center">
+      <select
+        name="inputYear" 
+        v-model="inputYear"
+        class="flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"
+      >
+         <option v-for="y, i in 100" :v-model="currentYear - i">{{ currentYear - i }}</option>
+      </select>
+    </div>
+    <div class="w-14">
+      <select
+        name="inputMonth"
+        v-model="inputMonth"
+        class="flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"
+      >
+        <option v-for="m in 12" :value="m">{{ m }}</option>
+      </select>
+    </div>
+    <div class="w-14">
+      <select
+        name="inputDate"
+        v-model="inputDate"
+        class="flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"
+      >
+        <option v-for="d in inputTotalDayOfMonth" :value="d">{{ d }}</option>
+      </select>
+    </div>
+    <div class="w-20">
+      <p>({{ inputDOW }})</p>
+    </div>
+   </div>
+</template>
+
+<script setup>
+  import dayName from "@/data/dayName.json"
+
+  const currentTime = new Date();
+  const currentYear = currentTime.getFullYear();
+  const currentMonth = currentTime.getMonth() + 1;
+  const currentDate = currentTime.getDate();
+
+  const inputYear = ref(currentYear);
+  const inputMonth = ref(currentMonth);
+  const inputDate = ref(currentDate);
+
+  const inputTotalDayOfMonth = computed(() => {
+    return new Date(inputYear.value, inputMonth.value, 0).getDate()
+  });
+
+  const inputDOW = computed(() => {
+    return dayName[new Date(inputYear.value, inputMonth.value - 1, inputDate.value).getDay()].shortName
+  });
+</script>
--- pages/member/new.vue
+++ pages/member/new.vue
@@ -1,39 +1,47 @@
 <template>
-  <!-- This is an example component -->
 <div class="min-h-screen flex items-center justify-center px-4">
-    
+                <div>{{date}}</div>
     <div class="max-w-4xl  bg-white w-full rounded-lg shadow-xl">
-        <div class="p-4 border-b">
-            <h2 class="text-2xl ">
-                Member Information
-            </h2>
-            <p class="text-sm text-gray-500">
-                Personal details and ranks. 
-            </p>
-        </div>
+      <div class="p-4 border-b">
+        <h2 class="text-2xl ">
+          Member Information
+        </h2>
+        <p class="text-sm text-gray-500">
+          Personal details and ranks. 
+        </p>
+      </div>
         <div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
                     Full name
                 </p>
-                <p>
-                    Jane Doe
-                </p>
+                <input type="text" id="class-name" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Full name"/>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
                     Sex
                 </p>
-                <p>
-                    Female
-                </p>
+                <ul class="items-center w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                  <li class="w-28 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                    <div class="flex items-center pl-3">
+                      <input id="one-time-class" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                      <label for="one-time-class" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Female</label>
+                    </div>
+                  </li>
+                  <li class="w-28 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                    <div class="flex items-center pl-3">
+                      <input id="repeat-class" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                      <label for="repeat-class" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Male</label>
+                    </div>
+                  </li>
+                </ul>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
                     Email Address
                 </p>
                 <p>
-                    Janedoe@gmail.com
+                  <input type="email" id="class-name" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Email address"/>
                 </p>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
@@ -41,171 +49,58 @@
                     Contact Number
                 </p>
                 <p>
-                    6012-2345689
+                  <input type="text" id="class-name" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Contact number"/>
                 </p>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-                <p class="text-gray-600">
-                    Birth Date
-                </p>
-                <p>
-                    1 April 2000
-                </p>
+              <p class="text-gray-600">
+                Birth Date (YYYY/MM/DD)
+              </p>
+              <inputDate/>
+            </div>
+            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p class="text-gray-600">
+                  Status
+              </p>
+              <select name="status" class="w-28 flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent">
+                <option value="Trial">Trial</option>
+                <option value="Enquiry">Enquiry</option>
+                <option value="Member">Member</option>
+              </select>
+            </div>
+            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p class="text-gray-600">
+                  Joined Date (YYYY/MM/DD)
+              </p>
+              <inputDate/>
+            </div>
+            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p class="text-gray-600">
+                  Contract End Date (YYYY/MM/DD)
+              </p>
+              <inputDate/>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
-                    Status
+                  Medical Issues
                 </p>
-                <p>
-                    Member
-                </p>
+                <textarea name="medicalIssues" id="" cols="30" rows="3" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"></textarea>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
-                    Joined Date
+                  Home Address
                 </p>
-                <p>
-                    20 April 2023
-                </p>
+                <textarea name="homeAddress" id="" cols="30" rows="3" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"></textarea>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
                 <p class="text-gray-600">
-                    Contract End Date
+                  Notes
                 </p>
-                <p>
-                    20 April 2024
-                </p>
-            </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-                <p class="text-gray-600">
-                    Monthly Payment
-                </p>
-                <div class="grid grid-cols-2 sm:grid-cols-3 grid-flow-row gap-4">
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Jan 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-green-200 text-green-900">
-                      Paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Feb 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-green-200 text-green-900">
-                      Paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Mar 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-green-200 text-green-900">
-                      Paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Apr 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-red-200 text-red-900">
-                      Due
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      May 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-red-200 text-red-900">
-                      Due
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Jun 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Jul 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Aug 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Sep 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Oct 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Nov 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                  <div class="w-fit border p-4">
-                    <div class="mb-2">
-                      Dec 2023
-                    </div>
-                    <div class="rounded-lg w-fit px-2 py-1 bg-slate-200 text-slate-900">
-                      Not paid
-                    </div>
-                  </div>
-                </div>
-            </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-                <p class="text-gray-600">
-                    Medical Issues
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                </p>
-            </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-                <p class="text-gray-600">
-                    Home Address
-                </p>
-                <p>
-                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu. 
-                </p>
-            </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-                <p class="text-gray-600">
-                    Notes
-                </p>
-                <p>
-                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu. 
-                </p>
+                <textarea name="notes" id="" cols="30" rows="3" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"></textarea>
             </div>
             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4">
                 <p class="text-gray-600">
-                    Signed Waiver
+                  Signed Waiver
                 </p>
                 <div class="space-y-2">
                     <div class="border-2 flex items-center p-2 rounded justify-between space-x-2">
@@ -224,8 +119,31 @@
 </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+  import dayName from "@/data/dayName.json"
 
+  const currentTime = new Date();
+  const currentYear = currentTime.getFullYear();
+  const currentMonth = currentTime.getMonth() + 1;
+  const currentDate = currentTime.getDate();
+
+  // Birthdate
+  const birthYear = ref(currentYear);
+  const birthMonth = ref(currentMonth);
+  const birthDate = ref(currentDate);
+
+  const birthTotalDayOfMonth = computed(() => {
+    return new Date(birthYear.value, birthMonth.value, 0).getDate()
+  });
+
+  const birthDOW = computed(() => {
+    return dayName[new Date(birthYear.value, birthMonth.value - 1, birthDate.value).getDay()].shortName
+  });
+
+  // Joined Date
+  // Contract End Date 
+
+  // console.log(dayName[new Date(birthYear.value, birthMonth.value - 1, birthDate.value).getDay()]);
 </script>
 
 <style scoped>
