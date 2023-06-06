430d2fd184aed87165c5b658c0b01e0d408deeaa
leongjs98 <leon_gjs@protonmail.com>
2023-06-07 00:12:16 +0800

    Added layout

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <header>
+    <nav>
+      <div class="">
+        <div class="flex justify-between h-16 px-10 shadow items-center">
+          <div class="flex items-center space-x-8">
+            <h1 class="text-xl lg:text-2xl font-bold cursor-pointer">Tailwind</h1>
+            <div class="hidden md:flex justify-around space-x-4">
+              <a href="#" class="hover:text-indigo-600 text-gray-700">Home</a>
+              <a href="#" class="hover:text-indigo-600 text-gray-700">About</a>
+              <a href="#" class="hover:text-indigo-600 text-gray-700">Service</a>
+              <a href="#" class="hover:text-indigo-600 text-gray-700">Contact</a>
+            </div>
+          </div>
+          <div class="flex space-x-4 items-center">
+            <a href="#" class="text-gray-800 text-sm">LOGIN</a>
+            <a href="#" class="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">SIGNUP</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+    <slot />
+  </div>
+</template>
