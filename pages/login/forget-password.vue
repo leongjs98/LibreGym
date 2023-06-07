b7261ccdd2a4b9569f8217d1d9af6fadb11b64b0
leongjs98 <leon_gjs@protonmail.com>
2023-06-08 01:17:55 +0800

    Update login page

--- pages/login/forget-password.vue
+++ pages/login/forget-password.vue
@@ -1,21 +1,4 @@
 <template>
-  <div class="h-screen bg-gray-100 flex justify-center">
-    <div class="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
-      <form action="">
-        <div>
-          <label for="email" class="block text-gray-800 font-bold">Email:</label>
-          <input type="text" name="email" id="email" placeholder="@email" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
-
-          <label for="password" class="block text-gray-800 font-bold">Password</label>
-          <input type="password" id="password" class="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600">
-
-          <a href="#" class="text-sm font-thin text-gray-800 hover:underline mt-2 inline-block hover:text-indigo-600">Forget Password</a>
-        </div>
-
-        <button class="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Login</button>
-      </form>
-    </div>
-  </div>
   <div class="container max-w-lg mx-auto h-screen flex flex-col justify-center">
     <div class="text-2xl text-center mx-3">
       Sign in to LibreGym to manage <br> <span class="font-semibold">Your Gym Name</span>
@@ -36,7 +19,7 @@
       </div>
     </form>
     <div class="text-center mx-3">
-      Please contact your administrator if you forgot your password.
+      FORGET PASSWORD
     </div>
   </div>
 </template>
