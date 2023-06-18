66cf8461cdaa030fefc499208451e957385f2411
leongjs98 <leon_gjs@protonmail.com>
2023-06-19 00:44:03 +0800

    rename URL
    `class` to `classes`
    `classes/new` to `class/create`

--- pages/classes/create.vue
+++ pages/classes/create.vue
@@ -11,7 +11,7 @@
               <p class="text-gray-600">
                   Class name
               </p>
-              <input type="text" id="class-name" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Class name"/>
+              <input type="text" class="w-full flex-1 appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Class name"/>
           </div>
           <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
               <p class="text-gray-600">
