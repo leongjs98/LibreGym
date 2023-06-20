9d29cee6e7acbf5d1543594e310054121c872003
leongjs98 <leon_gjs@protonmail.com>
2023-06-20 23:56:40 +0800

    Slight layout changes
    - Use CSS mobile dropdown instead of Javascript
    - Use `ul` and `li` tag for links

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,37 +6,39 @@
         <div class="flex justify-between h-16 px-10 shadow items-center">
           <div class="flex w-full justify-between items-center space-x-8">
             <h1 class="text-xl lg:text-2xl font-bold cursor-pointer">Gym Name</h1>
-            <div class="hidden md:flex justify-around space-x-4">
-              <NuxtLink href="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink>
-              <NuxtLink href="/attendance" class="hover:text-indigo-600 text-gray-700">Attendance</NuxtLink>
-              <NuxtLink href="/class" class="hover:text-indigo-600 text-gray-700">Class</NuxtLink>
-              <NuxtLink href="/member" class="hover:text-indigo-600 text-gray-700">Member</NuxtLink>
-              <!-- <NuxtLink href="/settings" class="hover:text-indigo-600 text-gray-700"> -->
+            <ul class="hidden md:flex justify-around space-x-4">
+              <li><NuxtLink to="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink></li>
+              <li><NuxtLink to="/attendances" class="hover:text-indigo-600 text-gray-700">Attendances</NuxtLink></li>
+              <li><NuxtLink to="/classes" class="hover:text-indigo-600 text-gray-700">Classes</NuxtLink></li>
+              <li><NuxtLink to="/members" class="hover:text-indigo-600 text-gray-700">Members</NuxtLink></li>
+              <!-- <NuxtLink to="/settings" class="hover:text-indigo-600 text-gray-700"> -->
               <!--   <Icon name="material-symbols:settings-outline" size="24px" /> -->
               <!-- Settings -->
               <!-- </NuxtLink> -->
-              <!-- <NuxtLink href="/profile" class="flex items-center gap-1 hover:text-indigo-600 text-gray-700"> -->
+              <!-- <NuxtLink to="/profile" class="flex items-center gap-1 hover:text-indigo-600 text-gray-700"> -->
               <!--   <Icon name="ic:baseline-person-outline" size="24px" /> -->
               <!-- Profile -->
               <!-- </NuxtLink> -->
-            </div>
+            </ul>
           </div>
           <div class="md:hidden flex space-x-4 items-center">
             <!-- <Icon name="icon-park:hamburger-button" color="black"/> -->
             <!-- <Icon name="uil:github" color="black" /> -->
-            <button @click="showMobileMenu = !showMobileMenu">
+            <label for="mobile-menu" class="hover:cursor-pointer">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </button>
+            </label>
           <!--   <a href="#" class="text-gray-800 text-sm">LOGIN</a> -->
           <!--   <a href="#" class="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">SIGNUP</a> -->
           </div>
         </div>
       </div>
-      <div v-show="showMobileMenu" class="fixed bg-gray-50 shadow w-full flex flex-col p-10 space-y-4">
-        <NuxtLink href="/attendance" class="hover:text-indigo-600 text-gray-700">Attendance</NuxtLink>
-        <NuxtLink href="/class" class="hover:text-indigo-600 text-gray-700">Class</NuxtLink>
-        <NuxtLink href="/member" class="hover:text-indigo-600 text-gray-700">Member</NuxtLink>
-      </div>
+      <input type="checkbox" id="mobile-menu" class="hidden peer">
+      <ul class="hidden peer-checked:flex fixed bg-gray-50 shadow w-full flex-col p-10 space-y-4">
+        <li><NuxtLink to="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink></li>
+        <li><NuxtLink to="/attendances" class="hover:text-indigo-600 text-gray-700">Attendances</NuxtLink></li>
+        <li><NuxtLink to="/classes" class="hover:text-indigo-600 text-gray-700">Classes</NuxtLink></li>
+        <li><NuxtLink to="/members" class="hover:text-indigo-600 text-gray-700">Members</NuxtLink></li>
+      </ul>
     </nav>
   </header>
   <!-- <div class="relative"> -->
@@ -56,7 +58,6 @@
 </template>
 
 <script setup lang="ts">
-const showMobileMenu = ref(false)
 </script>
 
 <style>
