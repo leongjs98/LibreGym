e1db65b2b3656f9ba44648a6c7489e6f2e20b183
leongjs98 <leon_gjs@protonmail.com>
2023-06-20 23:55:46 +0800

    Added pinia
    Added date store for dates in creating member

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   ],
   modules: [
     '@nuxtjs/tailwindcss',
-    'nuxt-icon'
+    'nuxt-icon',
+    '@pinia/nuxt',
   ],
   target:'static',
   plugins: [
---
+++ store/dateStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+
+export const useDateStore = defineStore('dateStore', () => {
+
+  const dates: Ref<{ birthday: string; joinedDate: string; contractEndDate: string }> = ref({
+    birthday: "2023-06-18T00:00:00.000Z",
+    joinedDate: "2023-06-19T00:00:00.000Z",
+    contractEndDate: "2023-06-20T00:00:00.000Z",
+  })
+
+  function setDate(propName: string, year: number, month: number, day: number) {
+    const yyyy = year
+    const mm = month < 10 ? `0${month}` : month
+    const dd = day < 10 ? `0${day}` : day
+
+    const computedDate = `${yyyy}-${mm}-${dd}`
+    // TODO: test propName and value validity
+    dates.value[propName] = computedDate + "T00:00:00.000Z"
+  }
+
+  return { dates, setDate }
+});
