22fa056dc5ff8212072eff9ce4aec5abe4322d7c
leongjs98 <leon_gjs@protonmail.com>
2023-07-30 17:47:37 +0800

    update composable and prisma scheme

--- composables/formatDate.ts
+++ composables/formatDate.ts
@@ -1,9 +1,16 @@
-export default function(date: Date): string {
-  // E.g. Jul 03 2023
+// E.g. Jul 03 2023
+export default function(date: string | Date | null | undefined): string {
+  if (!date) {
+    return ''
+  }
+
+  const dateObj = new Date(date)
+
   const options: Intl.DateTimeFormatOptions = {
     month: 'short',
     day: '2-digit',
     year: 'numeric'
   }
-  return date.toLocaleDateString('en-US', options).replace(',', '')
+
+  return dateObj.toLocaleDateString('en-US', options).replace(',', '')
 }
