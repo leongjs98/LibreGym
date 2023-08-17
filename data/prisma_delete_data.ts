cd9aafcc3bb9d6903a8ff4210086cde14345b248
leongjs98 <leon_gjs@protonmail.com>
2023-08-17 13:23:59 +0800

    ts script to reset DB

---
+++ data/prisma_delete_data.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log("Deleting data")
+  await prisma.attendance.deleteMany()
+  await prisma.session.deleteMany()
+  await prisma.member.deleteMany()
+  await prisma.class.deleteMany()
+}
+
+main()
+  .catch(e => {
+    console.error(e.message)
+  })
+  .finally(async () => {
+    console.log("Deletion completed...\nDisconnecting Prisma")
+    await prisma.$disconnect()
+  })
