7d893af8a261901073e97a75b5f430e5139f79e6
leongjs98 <leon_gjs@protonmail.com>
2023-09-02 22:45:31 +0800

    added notification UI
    simplify layout with better CSS

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,69 +1,150 @@
 <template>
   <div>
-  <header>
-    <nav>
-      <div class="">
-        <div class="flex justify-between h-16 px-10 shadow items-center">
-          <div class="flex w-full justify-between items-center space-x-8">
-            <h1 class="text-xl lg:text-2xl font-bold cursor-pointer">Gym Name</h1>
-            <ul class="hidden md:flex justify-around space-x-4">
-              <li><NuxtLink to="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink></li>
-              <li><NuxtLink to="/analytics" class="hover:text-indigo-600 text-gray-700">Analytics</NuxtLink></li>
-              <li><NuxtLink to="/classes" class="hover:text-indigo-600 text-gray-700">Classes</NuxtLink></li>
-              <li><NuxtLink to="/members" class="hover:text-indigo-600 text-gray-700">Members</NuxtLink></li>
-              <!-- <NuxtLink to="/settings" class="hover:text-indigo-600 text-gray-700"> -->
-              <!--   <Icon name="material-symbols:settings-outline" size="24px" /> -->
-              <!-- Settings -->
-              <!-- </NuxtLink> -->
-              <!-- <NuxtLink to="/profile" class="flex items-center gap-1 hover:text-indigo-600 text-gray-700"> -->
-              <!--   <Icon name="ic:baseline-person-outline" size="24px" /> -->
-              <!-- Profile -->
-              <!-- </NuxtLink> -->
-            </ul>
-          </div>
-          <div class="md:hidden flex space-x-4 items-center">
-            <!-- <Icon name="icon-park:hamburger-button" color="black"/> -->
-            <!-- <Icon name="uil:github" color="black" /> -->
-            <label for="mobile-menu" class="hover:cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </label>
-          <!--   <a href="#" class="text-gray-800 text-sm">LOGIN</a> -->
-          <!--   <a href="#" class="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">SIGNUP</a> -->
+    <div class="sticky top-0 w-full bg-slate-100 shadow">
+      <header
+        class="max-w-screen-xl flex mx-auto flex-row justify-between items-center bg-slate-100 z-50 p-4 sm:px-8 xl:px-0">
+        <h1 class="text-xl lg:text-2xl font-bold">Logo</h1>
+        <label for="menu-toggle" class="sm:hidden hover:cursor-pointer">
+          <Icon name="icon-park:hamburger-button" color="black" size="24px" />
+        </label>
+        <input type="checkbox" id="menu-toggle" class="peer hidden">
+        <nav
+          class="z-40 w-full absolute sm:static left-0 top-full origin-top scale-y-0 peer-checked:scale-y-100 sm:scale-y-100 transition-all duration-250 ease-in-out">
+          <ul class="flex items-end sm:justify-end gap-4 bg-slate-100 flex-col sm:flex-row px-4 pb-5 sm:p-0 text-lg">
+            <li class="opacity-0 transition-all duration-100 ease-in-out delay-0 sm:opacity-100">
+              <NuxtLink to="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink>
+            </li>
+            <li class="opacity-0 transition-all duration-100 ease-in-out delay-0 sm:opacity-100">
+              <NuxtLink to="/analytics" class="hover:text-indigo-600 text-gray-700">Analytics</NuxtLink>
+            </li>
+            <li class="opacity-0 transition-all duration-100 ease-in-out delay-0 sm:opacity-100">
+              <NuxtLink to="/classes" class="hover:text-indigo-600 text-gray-700">Classes</NuxtLink>
+            </li>
+            <li class="opacity-0 transition-all duration-100 ease-in-out delay-0 sm:opacity-100">
+              <NuxtLink to="/members" class="hover:text-indigo-600 text-gray-700">Members</NuxtLink>
+            </li>
+            <li class="opacity-0 transition-all duration-100 ease-in-out delay-0 sm:opacity-100">
+              <button @click="openNotification = !openNotification">
+                <Icon name="material-symbols:notifications-rounded" size="24px" />
+              </button>
+            </li>
+            <!-- <li> -->
+            <!--   <NuxtLink to="/settings" class="hover:text-indigo-600 text-gray-700"> -->
+            <!--     <Icon name="material-symbols:settings-outline" size="24px" /> -->
+            <!--     Settings -->
+            <!--   </NuxtLink> -->
+            <!-- </li> -->
+            <!-- <li> -->
+            <!-- <NuxtLink to="/profile" class="flex items-center gap-1 hover:text-indigo-600 text-gray-700"> -->
+            <!--   <Icon name="ic:baseline-person-outline" size="24px" /> -->
+            <!--   Profile -->
+            <!-- </NuxtLink> -->
+            <!-- </li> -->
+          </ul>
+        </nav>
+        <div v-if="openNotification"
+          class="z-50 absolute top-full left-0 w-full">
+          <div class="bg-slate-100 flex flex-col items-center gap-6 p-4 shadow shadow-gray-300">
+            <button @click="openNotification = false" type="button"
+              class="flex justify-center items-center border rounded-full p-0.5 border-slate-500">
+              <Icon name="clarity:collapse-line" size="24px" />
+            </button>
+            <!-- <p class="">There's no new notification...</p> -->
+            <div class="flex gap-2 text-lg">
+              <button @click="" type="button" class="" id="refresh-notification">
+                <Icon name="streamline:interface-arrows-synchronize-arrows-loading-load-sync-synchronize-arrow-reload"
+                  size="18px" />
+              </button>
+              <label for="refresh-notification" class="hover:cursor-pointer">
+                Check for Notification
+              </label>
+            </div>
+            <div class="divide-y divide-slate-500">
+              <div class="w-full flex flex-col py-4">
+                <div class="w-full flex justify-between">
+                  <header class="text-xl">Attendance streak!</header>
+                  <button type="button" class="">
+                    <IconCloseThick size="20px" />
+                  </button>
+                </div>
+                <p class="">
+                  XXX has attended X classes in the past month. Y classes per week.
+                </p>
+              </div>
+              <div class="w-full flex flex-col py-4">
+                <div class="w-full flex justify-between">
+                  <header class="text-xl">Birthday reminder</header>
+                  <button type="button" class="">
+                    <IconCloseThick size="20px" />
+                  </button>
+                </div>
+                <p class="">
+                  It's Z birthday on dd-mmm-yyyy. Wish them a happy birthday!
+                </p>
+              </div>
+              <div class="w-full flex flex-col py-4">
+                <div class="w-full flex justify-between">
+                  <header class="text-xl">Birthday reminder</header>
+                  <button type="button" class="">
+                    <IconCloseThick size="20px" />
+                  </button>
+                </div>
+                <p class="">
+                  It's Z birthday on dd-mmm-yyyy. Wish them a happy birthday!
+                </p>
+              </div>
+              <div class="w-full flex flex-col py-4">
+                <div class="w-full flex justify-between">
+                  <header class="text-xl">Due Payment</header>
+                  <button type="button" class="">
+                    <IconCloseThick size="20px" />
+                  </button>
+                </div>
+                <p class="">
+                  XXX membership was due on dd-mmm-yyyy (12 days ago)
+                </p>
+              </div>
+              <div class="w-full flex flex-col py-4">
+                <div class="w-full flex justify-between">
+                  <header class="text-xl">Inactive Member</header>
+                  <button type="button" class="">
+                    <IconCloseThick size="20px" />
+                  </button>
+                </div>
+                <p class="">
+                  XXX was last seen 49 days ago. contact them!
+                </p>
+              </div>
+            </div>
+            <div class="w-full flex justify-end">
+              <label for="more-notification" class="hover:cursor-pointer">
+                more
+              </label>
+              <button type="button" id="more-notification" class="">
+                <IconMore2Line size="24px" />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <input type="checkbox" id="mobile-menu" class="hidden peer">
-      <ul class="hidden peer-checked:flex fixed bg-gray-50 shadow w-full flex-col p-10 space-y-4">
-        <li><NuxtLink to="/" class="hover:text-indigo-600 text-gray-700">Home</NuxtLink></li>
-        <li><NuxtLink to="/analytics" class="hover:text-indigo-600 text-gray-700">Analytics</NuxtLink></li>
-        <li><NuxtLink to="/classes" class="hover:text-indigo-600 text-gray-700">Classes</NuxtLink></li>
-        <li><NuxtLink to="/members" class="hover:text-indigo-600 text-gray-700">Members</NuxtLink></li>
-      </ul>
-    </nav>
-  </header>
-  <!-- <div class="relative"> -->
-  <!-- <label for="menu-toggle" class="block px-3 py-2 bg-gray-200 text-gray-700 cursor-pointer"> -->
-  <!--   Menu -->
-  <!-- </label> -->
-  <!-- <input type="checkbox" id="menu-toggle" class="hidden"> -->
-  <!-- <div class="menu-content hidden absolute bg-white shadow-md mt-2 py-2 w-48 right-0"> -->
-  <!--   <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Home</a> -->
-  <!--   <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">About</a> -->
-  <!--   <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Services</a> -->
-  <!--   <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Contact</a> -->
-  <!-- </div> -->
-  <!-- </div> -->
+      </header>
+    </div>
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
+const openNotification = ref(false)
 </script>
 
-<style>
+<style scoped>
 .router-link-active {
   border-bottom-width: 2px;
   border-color: rgb(79, 20, 229);
   width: fit-content;
+}
+
+.peer:checked~nav li {
+  opacity: 1;
+  transition: opacity 150ms ease-in-out 200ms;
 }
 </style>
